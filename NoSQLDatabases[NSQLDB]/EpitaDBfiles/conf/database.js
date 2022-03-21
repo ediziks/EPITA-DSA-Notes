1a26286449a98b10fff5d5c09a186cb6b9a82209
zx <54022220+ediziks@users.noreply.github.com>
2022-03-21 13:29:35 +0100

    summary notes

--- NoSQLDatabases[NSQLDB]/EpitaDBfiles/conf/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URL, {family:4}, (error) => {
-    if (error) throw error;
-
-    console.log("Connected to database!");
-});
