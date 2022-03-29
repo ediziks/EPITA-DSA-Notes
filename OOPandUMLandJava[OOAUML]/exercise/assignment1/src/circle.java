8634fb4c78131b4816f2756700b34fa13f47b12d
zx <54022220+ediziks@users.noreply.github.com>
2022-03-29 23:56:03 +0200

    assgn changes

---
+++ OOPandUMLandJava[OOAUML]/exercise/assignment1/src/circle.java
@@ -0,0 +1,20 @@
+public class circle
+{
+    private static final double PI = 3.14159;
+    private double radius;
+
+    public circle (double r)
+    {
+        this.radius = r;
+    }
+
+    public double area()
+    {
+        return PI * radius * radius;
+    }
+
+    public double perimeter()
+    {
+        return 2 * PI * radius;
+    }
+}
