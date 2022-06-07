da2823fdfc492ad7a90921161c72a6b6404c8ba6
zx <54022220+ediziks@users.noreply.github.com>
2022-06-08 00:41:05 +0200

    class study

--- DataVisualization[DataViz]/ClassStudy/app.py
+++ DataVisualization[DataViz]/ClassStudy/app.py
@@ -3,51 +3,38 @@ import plotly.express as px
 import pandas as pd
 
 
+# initialize Dash app
 app = Dash(__name__)
 
-# read the df from csv from github
-# and drop the unnecessary columns
+# Read the df from csv from Github and drop the unnecessary columns
 df = pd.read_csv('https://gist.githubusercontent.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6/raw/92200bc0a673d5ce2110aaad4544ed6c4010f687/pokemon.csv')
 df = df.drop('Type 2', axis=1) # due to having many null values
 
-# generate df sample
-def generate_table(dataframe, max_rows=10):
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
-        ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))
-        ])
-    ])
-
-# figures to be plotted
+# Figures to be plotted
 fig = px.bar(df, x="Type 1", y="Total", barmode="group", color="Generation")
 fig2 = px.scatter(df, x="Name", y="Total", size="HP", color="Type 1")
 
-# HTML Layout
+# HTML Layout for the plots
 app.layout = html.Div(children=[
     html.Div([
-        html.H1(children='Pokemon Dashboard'),
-
-        html.H2(children='''
-            Dash: Graphs and Tables for Pokemon Data
-        '''),
-
-        html.Table([
-            html.H4(children='df Sample'),
-            generate_table(df)
-        ])
+        html.H1(children='Pokemon Spec Plots'),
+        html.P(children='Prepared by Ediz Emektas for Data Visualization class'),
     ]),
     html.Div([
+        html.H3(children='''
+            Plot 1: The total specs of Pokemons by their types and 
+            colorized by the generation of the each Pokemon
+        '''),
         dcc.Graph(
             id='example-graph',
             figure=fig
         )
     ]),
     html.Div([
+        html.H3(children='''
+            Plot 2: Each Pokemons' total specs (sum of all different skill points such as Power, HP, 
+            Attack), scatters are sized by the amount of their power, and colorized by their types
+        '''),
         dcc.Graph(
             id='example-graph-2',
             figure=fig2
